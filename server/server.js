ad320b723163ba2d3ace7bb7d828e0ad6cd2a705
2213473 <2224186@slu.edu.ph>
2026-03-27 14:55:04 +0800

    Add adminUsers controller and routes to manage user accounts

--- server/server.js
+++ server/server.js
@@ -11,9 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoute");
+const adminUsersRoute = require("./routes/adminUsersRoute");
 
 //Routes
 app.use("/auth", authRoutes);
+app.use("/users", adminUsersRoute);
 
 app.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}`);
