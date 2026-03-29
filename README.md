0d1f6f32f7c1a24a9f1b8c98ccf569b43050fd0d
cayeelii <carielmagajoyce@gmail.com>
2026-03-29 17:13:48 +0800

    Fixed import file and created README

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# DTR-Web
+# Philippine Military Academy (PMA) DTR Processing System
+
+**A web-based application that automates the management of employee Daily Time Records (DTR).**
+
+The application is divided into two main modules:
+
+- Admin Module – for managing and processing Daily Time Records (DTR) for employees
+- Employee Module – for viewing and printing individual DTR record
+
+## Tech Stack
+![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB) ![MySQL](https://img.shields.io/badge/mysql-4479A1.svg?style=for-the-badge&logo=mysql&logoColor=white)
+
+
--- frontend/src/components/ImportFile.jsx
+++ frontend/src/components/ImportFile.jsx
@@ -9,7 +9,7 @@ const ImportFile = ({ onUpload }) => {
   };
 
   return (
-    <div className="bg-white rounded-xl shadow-sm p-6 md:p-8 w-full border border-gray-200">
+    <div className="bg-white rounded-xl shadow-sm p-12 md:p-10 w-full border border-gray-200">
       <h2 className="text-lg md:text-xl font-bold text-gray-800 mb-6">Upload DTR File</h2>
 
       <div className="border-2 border-dashed border-gray-200 rounded-2xl py-16 md:py-20 flex flex-col items-center justify-center bg-[#F8F9FB] hover:bg-gray-100 transition-all cursor-pointer relative group">
@@ -19,10 +19,7 @@ const ImportFile = ({ onUpload }) => {
           onChange={handleFileChange} 
           accept=".xlsx, .xls"
         />
-        
-        <div className="bg-white p-4 rounded-full shadow-md mb-4 group-hover:scale-110">
           <CloudDownload className="w-12 h-12 text-blue-900" />
-        </div>
         
         <p className="text-gray-500 font-medium text-center">Import your file</p>
       </div>
