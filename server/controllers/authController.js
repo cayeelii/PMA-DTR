3dc49749bce691d4c1bf2eed74cfcb69ce25fdf4
Syak-niIAN <2223900@slu.edu.ph>
2026-04-11 15:38:20 +0800

    changepass server side

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -247,10 +247,69 @@ const getCurrentUser = (req, res) => {
   }
 };
 
+//Change Password
+const changePassword = async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+
+    const userId = req.session.user?.user_id; 
+
+    if (!userId) {
+      return res.status(401).json({ message: "Not logged in" });
+    }
+
+    db.query(
+      "SELECT * FROM users WHERE user_id = ?",
+      [userId],
+      async (err, results) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ message: "Database error" });
+        }
+
+        if (results.length === 0) {
+          return res.status(404).json({ message: "User not found" });
+        }
+
+        const user = results[0];
+
+        const isMatch = await bcrypt.compare(currentPassword, user.password);
+
+        if (!isMatch) {
+          return res.status(400).json({
+            message: "Current password is incorrect",
+          });
+        }
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+        db.query(
+          "UPDATE users SET password = ? WHERE user_id = ?",
+          [hashedPassword, userId],
+          (err) => {
+            if (err) {
+              console.error(err);
+              return res.status(500).json({
+                message: "Error updating password",
+              });
+            }
+
+            res.json({ message: "Password changed successfully" });
+          }
+        );
+      }
+    );
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
 module.exports = {
   register,
   adminLogin,
   employeeLogin,
   logout,
   getCurrentUser,
+  changePassword,
 };
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const authController = require("../controllers/authController");
+const { changePassword } = require("../controllers/authController");
+
+
+//POST /auth/change-password
+router.post("/change-password", changePassword);
 
 //POST /auth/register
 router.post("/register", authController.register);
